aed4cc8caa36f2920a2a245aba50a16b17c3ee9a
박지민 <pjmlove2008@gmail.com>
2024-11-04 19:38:24 +0900

    프로젝트 세팅 :: 추가

---
+++ Lezhin/src/test/java/org/example/lezhin/LezhinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lezhin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LezhinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
